ecda1229d471cc430749131d1eb8a4418f2365e2
Hong-D <2236710058@qq.com>
2022-05-28 13:21:01 +0800

    'demo'

---
+++ pages/my/commentList.js
@@ -0,0 +1,15 @@
+var app = getApp();
+Page({
+    data: {
+        list: [
+           
+        ]
+    },
+    onLoad: function (options) {
+        // 生命周期函数--监听页面加载
+
+    },
+    onShow: function () {
+        var that = this;
+    }
+});
